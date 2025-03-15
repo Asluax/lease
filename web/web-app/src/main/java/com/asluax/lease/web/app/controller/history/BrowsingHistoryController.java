7fb555d3dd57456a796b0e496b90d50d23936cda
Asluax <asluax@gmail.com>
2025-03-15 11:50:47 +0800

    尚庭公寓制作完成(成功部署)

--- web/web-app/src/main/java/com/asluax/lease/web/app/controller/history/BrowsingHistoryController.java
+++ web/web-app/src/main/java/com/asluax/lease/web/app/controller/history/BrowsingHistoryController.java
@@ -14,9 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
 @RestController
 @Tag(name = "浏览历史管理")
 @RequestMapping("/app/history")
